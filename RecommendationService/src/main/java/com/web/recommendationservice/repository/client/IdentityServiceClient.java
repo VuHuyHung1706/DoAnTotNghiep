7c7dd834ba4388311f1c446cbdc692688268c205
Vu Huy Hung <vhh1706@Vus-MacBook-Air.local>
2025-12-26 18:55:40 +0700

    deploy

--- RecommendationService/src/main/java/com/web/recommendationservice/repository/client/IdentityServiceClient.java
+++ RecommendationService/src/main/java/com/web/recommendationservice/repository/client/IdentityServiceClient.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "identity-service", url = "http://localhost:8080")
+@FeignClient(name = "identity-service", url = "http://172.30.1.10:8080")
 public interface IdentityServiceClient {
     @PostMapping("/auth/introspect")
     ApiResponse<IntrospectResponse> introspect(@RequestBody IntrospectRequest request);
